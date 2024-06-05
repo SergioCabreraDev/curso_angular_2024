84b01fcabdaa5259b4b225b641009e60fe313d86
Sergio Cabrera <scabrera.main@gmail.com>
2024-06-05 07:48:21 +0200

    userapp

--- 4.Carrito Compras - Redux/carrito-compra/src/app/store/effects/products.effects.ts
+++ 4.Carrito Compras - Redux/carrito-compra/src/app/store/effects/products.effects.ts
@@ -9,19 +9,16 @@ import { findAll, load } from "../products.actions";
 export class ProductsEffects {
 
 
-    loadProduct$ = createEffect(  // Definimos un efecto con createEffect
-    () => this.actions$.pipe(  // La función flecha devuelve un observable transformado por una cadena de operadores RxJS
-        ofType(load),  // El operador ofType filtra las acciones, permitiendo solo las de tipo 'load'
-        exhaustMap(() =>  // El operador exhaustMap maneja la llamada asíncrona, desechando nuevas acciones 'load' si una ya está en progreso
-            this.service.findAll().pipe(  //Llamamos al método findAll del servicio que devuelve un Observable<Product[]>
+    loadProduct$ = createEffect(  // Definimos un efecto con createEffect, reacciona a acciones despaches
+        () => this.actions$.pipe(  // this.actions$ es un observable que emite todas las acciones despachadas en la aplicación.
+            ofType(load),  // El operador ofType filtra las acciones, permitiendo solo las de tipo 'load'
+            exhaustMap(() => this.service.findAll()  // El operador exhaustMap maneja la llamada asíncrona, desechando nuevas acciones 'load' si una ya está en progreso  //Llamamos al método findAll del servicio que devuelve un Observable<Product[]>
+            .pipe(
                 map(products => findAll({ products }))  // Transformamos el Observable<Product[]> en una acción findAll con los productos
             )
+            )
         )
-    )
-);
-
-    
-    
+    );
 
     constructor(
         private actions$: Actions,
