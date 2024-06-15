1c2b703ec9b2160d712eb9c128b4b3b195b6059b
Sergio Cabrera <scabrera.main@gmail.com>
2024-06-15 17:02:28 +0200

    sdfsd

--- 5.UserAppRouter/user-app/src/app/services/user.service.ts
+++ 5.UserAppRouter/user-app/src/app/services/user.service.ts
@@ -1,40 +1,17 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private users: User[] = [{
-    id:1,
-    name: 'andres',
-    lastname: 'guzman',
-    email: 'andres@gmail.com',
-    username: 'andres11',
-    password: '12343242343'
-  },
-  {
-    id:2,
-    name: 'lucas',
-    lastname: 'mendo',
-    email: 'lucas@gmail.com',
-    username: 'lucas756',
-    password: '1233242323'
-  },
-  {
-    id:3,
-    name: 'sergio',
-    lastname: 'cabra',
-    email: 'sergio@gmail.com',
-    username: 'sergio322',
-    password: '12345676575'
-  }
-];
-  constructor() { }
+  private users: User[] = []
+  constructor(private http: HttpClient) { }
 
   findAll(): Observable<User[]>{
-    return of(this.users);
+    return this.http.get<User[]>('http://localhost:8080/api/users');
   }
 }
