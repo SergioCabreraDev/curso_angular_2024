27b8eb0ab672d27aac670b292c6b11345de23865
Sergio Cabrera <scabrera.main@gmail.com>
2024-07-12 21:13:38 +0200

    backend

--- 6. Parte SpringBoot/users-backend/src/main/java/com/springboot/backend/sergio/userapp/users_backend/auth/SpringSecurityConfig.java
+++ 6. Parte SpringBoot/users-backend/src/main/java/com/springboot/backend/sergio/userapp/users_backend/auth/SpringSecurityConfig.java
@@ -1,15 +1,36 @@
 package com.springboot.backend.sergio.userapp.users_backend.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.springboot.backend.sergio.userapp.users_backend.auth.filter.JwtAuthenticationFilter;
 
 // Marca esta clase como una clase de configuración para Spring
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration autenticacionConfiguration;
+
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return autenticacionConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     // Define un bean de tipo SecurityFilterChain, que es la cadena de filtros de seguridad de Spring Security
     @Bean
@@ -27,6 +48,7 @@ public class SpringSecurityConfig {
                     // Requiere autenticación para cualquier otra solicitud
                     .anyRequest().authenticated()
             )
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
             // Desactiva la protección CSRF (Cross-Site Request Forgery)
             .csrf(config -> config.disable())
             // Configura la gestión de sesiones para que no se cree ninguna sesión (stateless)
--- 6. Parte SpringBoot/users-backend/src/main/java/com/springboot/backend/sergio/userapp/users_backend/auth/filter/JwtAuthenticationFilter.java
+++ 6. Parte SpringBoot/users-backend/src/main/java/com/springboot/backend/sergio/userapp/users_backend/auth/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.springboot.backend.sergio.userapp.users_backend.auth.filter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
@@ -13,10 +18,13 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.backend.sergio.userapp.users_backend.entities.User;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import static com.springboot.backend.sergio.userapp.users_backend.auth.TokenJwtConfig.*;
 
 // Clase JwtAuthenticationFilter que extiende UsernamePasswordAuthenticationFilter para manejar la autenticación basada en JWT
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -68,8 +76,36 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        super.successfulAuthentication(request, response, chain, authResult);
+        
+            org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User)authResult.getPrincipal();
+            String username = user.getUsername();
+
+            Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+            Claims claims = Jwts.claims()
+            .add("authorities", new ObjectMapper().writeValueAsString(roles))
+            .add("username", username).build();
+ 
+            String jwt = Jwts.builder()
+                    .subject(username)
+                    .claims(claims)
+                    .signWith(SECRET_KEY)
+                    .issuedAt(new Date())
+                    .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .compact();
+
+            response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + jwt);
+
+            Map<String, String> body = new HashMap<>();
+            body.put("token", jwt);
+            body.put("username", username);
+            body.put("messsage", String.format("HAS %s INICIADO SESION CON EXITO", username));
+
+            response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+            response.setContentType(CONTENT_TYPE);
+
+            response.setStatus(200);
+
     }
 
     @Override
